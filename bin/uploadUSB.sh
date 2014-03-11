29acf225960700eeacb914b228606935f766d1f0
jjanga <jjanga@jjangaui-MacBook.local>
2014-03-12 05:04:55 +0900

    2 switch input(a/b) test

--- bin/uploadUSB.sh
+++ bin/uploadUSB.sh
@@ -1,4 +1,4 @@
 sudo dfu-programmer at90usb82 erase
-sudo dfu-programmer at90usb82 flash --debug 1 Arduino-usbserial.hex
+sudo dfu-programmer at90usb82 flash --debug 1 Arduino-usbserial-uno.hex
 sudo dfu-programmer at90usb82 reset
 
--- src/footswitch/footswitch.ino
+++ src/footswitch/footswitch.ino
@@ -2,17 +2,16 @@ uint8_t buf[8] = {0};
 
 const int LEFT_CTRL = 0xE0;
 const int LEFT_ALT  = 0xE2;
+const int KEYCODE_A = 0x04;
 const int KEYCODE_B = 0x05;
 
 
-const int pedalPin1 = 1;
-const int pedalPin2 = 2;
-const int ledPin1 = 3;
+const int pedalPin1 = 2;
+const int pedalPin2 = 3;
+const int ledPin1 = 4;
 
-int pedalState1 = 0;
-int pedalState2 = 0;
-
-int pedalState2Before = 1;
+int lastPedalEvent1 = 0;
+int lastPedalEvent2 = 0;
 
 int DBG_MODE = 0;
 
@@ -20,7 +19,7 @@ int DBG_MODE = 0;
 void setup()
 {
   Serial.begin(9600);
-//  pinMode(pedalPin1, INPUT);
+  pinMode(pedalPin1, INPUT);
   pinMode(pedalPin2, INPUT);
   pinMode(ledPin1, OUTPUT);
   
@@ -28,33 +27,23 @@ void setup()
 
 void loop()
 {
-  
-//  pedalState1 = digitalRead(pedalPin1);
-  pedalState2 = digitalRead(pedalPin2);
-  
-  if(pedalState2 == pedalState2Before) {
+  int pedalEvent1 = digitalRead(pedalPin1);
+  int pedalEvent2 = digitalRead(pedalPin2);
+
+  if((lastPedalEvent1 == pedalEvent1) && (lastPedalEvent2 == pedalEvent2)) {
     return;
   }
   
-  if(pedalState2Before > 0) releaseKey();
-  
-  pedalState2Before = pedalState2;  
-    
-  if((pedalState2 == HIGH)) {
-    if(DBG_MODE) {
-      Serial.print("pedal[0][1]==[");
-      Serial.print(pedalState1);
-      Serial.print("][");
-      Serial.print(pedalState2);      
-      Serial.println("]");      
-    }
-      if(pedalState1 == HIGH) {
-        input(LEFT_ALT);
-      } else if(pedalState2 == HIGH) {
-        input(LEFT_CTRL);
-      }
-      digitalWrite(ledPin1, HIGH);
+  if((pedalEvent1 == HIGH) || (pedalEvent2 == HIGH)) {
+    if(pedalEvent1 == HIGH) input(KEYCODE_A);
+    if(pedalEvent2 == HIGH) input(KEYCODE_B);
+    lastPedalEvent1 = pedalEvent1;
+    lastPedalEvent2 = pedalEvent2;
+    digitalWrite(ledPin1, HIGH);
   } else {
+    releaseKey();
+    lastPedalEvent1 = pedalEvent1;
+    lastPedalEvent2 = pedalEvent2;    
     digitalWrite(ledPin1, LOW);
   }
 }
